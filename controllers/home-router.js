3458cb530f97c383abe3591e44f0496cf166a5ba
Patrick McKnight <patrickgmcknight@gmail.com>
2023-04-12 17:59:00 -0700

    added homepage and single-post views, created single-post, login, and signup routes

--- controllers/home-router.js
+++ controllers/home-router.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const sequelize = require("../config/connection");
 const { Post, User, Comment } = require("../models");
 
-// get all posts for homepage
+// get all posts and render homepage
 router.get("/", (req, res) => {
   console.log(req.session);
 
@@ -39,3 +39,69 @@ router.get("/", (req, res) => {
       res.status(500).json(err);
     });
 });
+
+// render single post page
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    attributes: ["id", "post_content", "title", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// render login page
+router.get("/login", (req, res) => {
+  if (req.session.loggedIn) {
+    // if user is already logged in, redirect to homepage
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+});
+
+// render signup page
+router.get("/signup", (req, res) => {
+  if (req.session.loggedIn) {
+    // if user is already logged in, redirect to homepage
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');});
+
+module.exports = router;
