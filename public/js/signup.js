99972d5c1bb93e569c306e285b0436031558bd3e
Patrick McKnight <patrickgmcknight@gmail.com>
2023-04-15 21:01:56 -0700

    created frontend signup handler function

---
+++ public/js/signup.js
@@ -0,0 +1,25 @@
+async function signupHandler (event) {
+    event.preventDefault();
+    const username = document.querySelector('#usernameInput').value.trim();
+    const password = document.querySelector('#passwordInput').value.trim();
+    const email = document.querySelector('#emailInput').value.trim();
+    if (username && password && email) {
+        const response = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password,
+                email
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+        if (response.ok) {
+            alert('Account created! Welcome to The Tech Blog!');
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+}
+
+doucment.querySelector('#signupForm').addEventListener('submit', signupHandler);
